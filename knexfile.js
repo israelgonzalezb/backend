a11281c8c8766bbb1e69f8e5310587e9565452a2
unknown <will.ediger@gmail.com>
2019-09-04 21:00:12 -0500

    built knexfile and db config

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/lifeGPADevelopment.db3"
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/lifeGPATesting.db3"
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
+};
