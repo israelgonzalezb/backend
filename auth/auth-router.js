dd0cfeab38063e4ee074dfffd651ac5561a8a54e
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-09-07 03:25:04 -0700

    Refactored auth-routr to use async/await

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,47 +1,63 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-//const secrets = require("../config/secrets.js");
 
 const Users = require("../users/users-model.js");
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
     console.log("on register route");
     let user = req.body;
     const hash = bcrypt.hashSync(user.password, 10);
     user.password = hash;
-
-    Users.add(user)
-    .then(saved => {
-        res.status(201).json(saved);
-    })
-    .catch(err => {
-        res.status(500).json(err);
-    });
+    try{
+      const newUser = await Users.add(user);
+      res.status(201).json(newUser);
+    } catch(err) {
+      res.status(500).json(err);
+    }
 });
 
-router.post('/login', (req, res, next) => {
+router.post('/login', async (req, res, next) => {
     let { username, password } = req.body;
     
-    Users.findBy(username)
-      .first()
-      .then(user => {
+    
+    try {
+      const user = await Users.findBy(username).first();
+      //const userExists = Object.keys(user).length;
+      //console.log(`Pass is correct? ${correctPassword}`);
+      if (user && bcrypt.compareSync(password, user.password)){
+        req.session.loggedIn = true;
+        // do we need full user object in session?
+        // or should we just keep the username in the session, for safer infosec pattern?
+        req.session.user = user;
+        res.status(200).json({message: `Welcome ${user.username}!`});
+        next();
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    } catch(err) {
+      console.log(err);
+      res.status(500).json(err);
+    };
+    // Users.findBy(username)
+    //   .first()
+    //   .then(user => {
           
-        if (user && bcrypt.compareSync(password, user.password)) {
-          req.session.loggedIn = true;
-          // do we need to keep this as the full user object in the session
-          // or should we just keep the username in the session, for safer infosec pattern?
-          req.session.user = user;
-          res.status(200).json({message: `Welcome ${user.username}!`});
-          next();
+    //     if (user && bcrypt.compareSync(password, user.password)) {
+    //       req.session.loggedIn = true;
+    //       // do we need to keep this as the full user object in the session
+    //       // or should we just keep the username in the session, for safer infosec pattern?
+    //       req.session.user = user;
+    //       res.status(200).json({message: `Welcome ${user.username}!`});
+    //       next();
   
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
+    //     } else {
+    //       res.status(401).json({ message: 'Invalid Credentials' });
+    //     }
+    //   })
+    //   .catch(error => {
+    //     res.status(500).json(error);
+    //   });
   });
   
 
