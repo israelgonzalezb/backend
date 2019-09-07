a6f1eeb75dd8a7104d72aa626f5d7c1634ef8392
unknown <will.ediger@gmail.com>
2019-09-06 19:58:29 -0500

    built habit tracking seeds

---
+++ data/seeds/005-habit-tracking.js
@@ -0,0 +1,69 @@
+exports.seed = function(knex) {
+  return knex("User_Habits").insert([
+    {
+      user_habit_id: 1,
+      done_on: new Date("January 31 2019 12:30"),
+      quantity: 1
+    },
+    {
+      user_habit_id: 1,
+      done_on: new Date("February 2 2019 12:30"),
+      quantity: 2
+    },
+    {
+      user_habit_id: 2,
+      done_on: new Date("January 31 2019 13:30"),
+      quantity: 2
+    },
+    {
+      user_habit_id: 3,
+      done_on: new Date("January 31 2019 16:30"),
+      quantity: 1
+    },
+    {
+      user_habit_id: 4,
+      done_on: new Date("January 31 2019 9:30"),
+      quantity: 2
+    },
+    {
+      user_habit_id: 4,
+      done_on: new Date("February 2 2019 8:30"),
+      quantity: 2
+    },
+    {
+      user_habit_id: 5,
+      done_on: new Date("January 31 2019 12:00"),
+      quantity: 1
+    },
+    {
+      user_habit_id: 5,
+      done_on: new Date("January 31 2019 12:00"),
+      quantity: 1
+    },
+    {
+      user_habit_id: 6,
+      done_on: new Date("January 31 2019 16:00"),
+      quantity: 1.5
+    },
+    {
+      user_habit_id: 7,
+      done_on: new Date("January 31 2019 16:15"),
+      quantity: 1
+    },
+    {
+      user_habit_id: 8,
+      done_on: new Date("January 31 2019 18:15"),
+      quantity: 2
+    },
+    {
+      user_habit_id: 9,
+      done_on: new Date("January 31 2019 14:15"),
+      quantity: 1
+    },
+    {
+      user_habit_id: 10,
+      done_on: new Date("January 31 2019 19:30"),
+      quantity: 1.5
+    }
+  ]);
+};
