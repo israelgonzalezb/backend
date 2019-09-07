34a27eb6e1c93ceefd7ad6d025f093e893a3d815
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-09-06 20:56:47 -0700

    Removed duplicate loggers/errHandler middleware

--- api/server.js
+++ api/server.js
@@ -6,12 +6,14 @@ const session = require("express-session");
 const usersRouter = require("../users/users-router.js");
 const userCategoriesRouter = require("../user-categories/user-categories-router.js");
 
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(logger);
 server.use(cors());
+server.use(errHandler);
 server.use(
   session({
     name: "LifeGPA", // default is connect.sid
@@ -25,26 +27,12 @@ server.use(
     saveUninitialized: false
   })
 );
+server.use("/api/users", usersRouter);
+server.use("/api/user_categories", userCategoriesRouter);
 
 server.get("/", (req, res) => {
   res.send("Server up and running...");
 });
-
-//allows you to pass object of {status: ..., message: ...} to next, and the middleware sends out a json response with that data
-function errHandler(err, req, res, next) {
-  res.status(err.status).json({ message: err.message });
-}
-
-//logs out method and url of each request
-function logger(req, res, next) {
-  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
-  next();
-}
-
-server.use("/api/users", usersRouter);
-server.use("/api/user_categories", userCategoriesRouter);
-
-server.use(errHandler);
 
 //allows you to pass object of {status: ..., message: ...} to next, and the middleware sends out a json response with that data
 function errHandler(err, req, res, next) {
