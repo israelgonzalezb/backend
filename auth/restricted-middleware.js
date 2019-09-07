239e0ee2ac64d81a250528b7d1b1ccc08e2081c8
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-09-06 19:36:24 -0700

    Updated restricted-middleware to use sessions

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,6 +1,9 @@
-const Users = require("../users/users-model.js");
 
+// check request body session.loggedIn boolean
+// this is set on auth-router on the login endpoint
 module.exports = (req, res, next) => {
-  // TODO: need to build this out next
-  next();
-};
+    const sesh = req.session;
+    (sesh && (sesh.loggedIn === true))
+    ? next()
+    : res.status(400).json({message: "You're not allowed in here!"});
+  }
