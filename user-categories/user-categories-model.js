fb0a3d70c9f8b19998f27daa7ecd993cc9a718ba
unknown <will.ediger@gmail.com>
2019-09-09 22:38:57 -0500

    fixed references to "findById" to getById. set "remove" return deleted object

--- user-categories/user-categories-model.js
+++ user-categories/user-categories-model.js
@@ -1,33 +1,43 @@
 const db = require("../data/dbConfig.js");
 
 module.exports = {
-  getAll: function() {
-    return db("User_Categories");
-  },
-  getByUserId: function(user_id) {
-    return db("User_Categories")
-      .select("id", "category_id", "weight")
-      .where("user_id", user_id);
-  },
-  getById: function(id) {
-    return db("User_Categories")
-      .where("id", id)
-      .first();
-  },
-  insert: function(user_category) {
-    return db("User_Categories")
-      .insert(user_category)
-      .then(([id]) => this.findById(id));
-  },
-  update: function(updated_user_category, id) {
-    return db("User_Categories")
-      .where("id", id)
-      .update(updated_user_category)
-      .then(count => (count > 0 ? this.findById(id) : null));
-  },
-  remove: function(id) {
-    return db("User_Categories")
-      .where("id", id)
-      .del();
-  }
+  getAll,
+  getByUserId,
+  getById,
+  insert,
+  update,
+  remove
 };
+
+function getAll() {
+  return db("User_Categories");
+}
+function getByUserId(user_id) {
+  return db("User_Categories")
+    .select("id", "category_id", "weight")
+    .where("user_id", user_id);
+}
+function getById(id) {
+  return db("User_Categories")
+    .where("id", id)
+    .first();
+}
+function insert(user_category) {
+  return db("User_Categories")
+    .insert(user_category)
+    .then(([id]) => this.getById(id));
+}
+function update(updated_user_category, id) {
+  return db("User_Categories")
+    .where("id", id)
+    .update(updated_user_category)
+    .then(count => (count > 0 ? this.getById(id) : null));
+}
+
+async function remove(id) {
+  const userCategory = await getById(id);
+  await db("User_Categories")
+    .where("id", id)
+    .del();
+  return userCategory;
+}
