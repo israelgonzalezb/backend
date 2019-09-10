308143d76e08b430c577936aefdfbff837e8cb16
unknown <will.ediger@gmail.com>
2019-09-09 23:40:23 -0500

    added spaces in user categories model

--- user-categories/user-categories-model.js
+++ user-categories/user-categories-model.js
@@ -12,21 +12,25 @@ module.exports = {
 function getAll() {
   return db("User_Categories");
 }
+
 function getByUserId(user_id) {
   return db("User_Categories")
     .select("id", "category_id", "weight")
     .where("user_id", user_id);
 }
+
 function getById(id) {
   return db("User_Categories")
     .where("id", id)
     .first();
 }
+
 function insert(user_category) {
   return db("User_Categories")
     .insert(user_category)
     .then(([id]) => this.getById(id));
 }
+
 function update(updated_user_category, id) {
   return db("User_Categories")
     .where("id", id)
