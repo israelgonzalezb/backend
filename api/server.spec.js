f59ed7f0e7a16e0abef32be8c3e47ba1dd8d3a20
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-09-11 18:33:56 -0700

    Implemented tests for GET /api/ routes

--- api/server.spec.js
+++ api/server.spec.js
@@ -192,3 +192,158 @@ describe("server.js", () => {
     });
   });
 });
+
+// Test GET api/$route
+const expectedUserCategories = [
+  { category_id: 1, id: 1, user_id: 1, weight: 0.3 },
+  { category_id: 2, id: 2, user_id: 1, weight: 0.7 },
+  { category_id: 1, id: 3, user_id: 2, weight: 0.9 },
+  { category_id: 2, id: 4, user_id: 2, weight: 0.1 }
+];
+
+const expectedUserHabits = [
+  {
+    category_id: 1,
+    daily_goal_amount: "100 g",
+    description: null,
+    id: 1,
+    name: "eat broccoli",
+    user_id: 1,
+    weight: 0.25
+  },
+  {
+    category_id: 1,
+    daily_goal_amount: "75 g",
+    description: null,
+    id: 2,
+    name: "eat carrots",
+    user_id: 1,
+    weight: 0.25
+  },
+  {
+    category_id: 1,
+    daily_goal_amount: "1 mile",
+    description: null,
+    id: 3,
+    name: "walk",
+    user_id: 1,
+    weight: 0.5
+  },
+  {
+    category_id: 2,
+    daily_goal_amount: "1 cup",
+    description: null,
+    id: 4,
+    name: "make my own coffee in the morning",
+    user_id: 1,
+    weight: 0.4
+  },
+  {
+    category_id: 2,
+    daily_goal_amount: "once per day",
+    description: null,
+    id: 5,
+    name: "bring my own lunch",
+    user_id: 1,
+    weight: 0.6
+  },
+  {
+    category_id: 1,
+    daily_goal_amount: "10",
+    description: null,
+    id: 6,
+    name: "do pushups",
+    user_id: 2,
+    weight: 0.25
+  },
+  {
+    category_id: 1,
+    daily_goal_amount: "25",
+    description: null,
+    id: 7,
+    name: "do situps",
+    user_id: 2,
+    weight: 0.25
+  },
+  {
+    category_id: 1,
+    daily_goal_amount: "2 miles",
+    description: null,
+    id: 8,
+    name: "walk",
+    user_id: 2,
+    weight: 0.5
+  },
+  {
+    category_id: 2,
+    daily_goal_amount: "daily",
+    description: null,
+    id: 9,
+    name: "record expenses in budget",
+    user_id: 2,
+    weight: 0.2
+  },
+  {
+    category_id: 2,
+    daily_goal_amount: "30 minutes",
+    description: null,
+    id: 10,
+    name: "work on business ideas",
+    user_id: 2,
+    weight: 0.8
+  }
+];
+
+const expectedHabitTracking = [
+  { done_on: 1548966600000, id: 1, quantity: 1, user_habit_id: 1 },
+  { done_on: 1549139400000, id: 2, quantity: 2, user_habit_id: 1 },
+  { done_on: 1548970200000, id: 3, quantity: 2, user_habit_id: 2 },
+  { done_on: 1548981000000, id: 4, quantity: 1, user_habit_id: 3 },
+  { done_on: 1548955800000, id: 5, quantity: 2, user_habit_id: 4 },
+  { done_on: 1549125000000, id: 6, quantity: 2, user_habit_id: 4 },
+  { done_on: 1548964800000, id: 7, quantity: 1, user_habit_id: 5 },
+  { done_on: 1548964800000, id: 8, quantity: 1, user_habit_id: 5 },
+  { done_on: 1548979200000, id: 9, quantity: 1.5, user_habit_id: 6 },
+  { done_on: 1548980100000, id: 10, quantity: 1, user_habit_id: 7 },
+  { done_on: 1548987300000, id: 11, quantity: 2, user_habit_id: 8 },
+  { done_on: 1548972900000, id: 12, quantity: 1, user_habit_id: 9 },
+  { done_on: 1548991800000, id: 13, quantity: 1.5, user_habit_id: 10 }
+];
+
+describe.each`
+  route                | expected
+  ${"user_categories"} | ${expectedUserCategories}
+  ${"user_habits"}     | ${expectedUserHabits}
+  ${"habit_tracking"}  | ${expectedHabitTracking}
+`("GET api/$route", ({ route, expected }) => {
+  const contextClassRef = ContextHelper;
+  beforeAll(async () => {
+    try {
+      await db.seed.run();
+      const authTest = await request(server)
+        .post("/api/users/login")
+        .send(testUser);
+      contextClassRef.session = authTest.header["set-cookie"];
+    } catch (err) {
+      console.log(`Error on test login! ${err}`);
+    }
+  });
+  it(`when logged-in, should return ${expected}`, async () => {
+    console.log(`**** ${contextClassRef.session} ****`);
+
+    const response = await request(server)
+      .get(`/api/${route}`)
+      .set("Cookie", contextClassRef.session);
+    //console.log(response);
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual(expected);
+    expect(response.type).toEqual("application/json");
+  });
+  it(`when logged-out, should return {"message":"You're not allowed in here!"}`, async () => {
+    const response = await request(server).get(`/api/${route}`);
+    const restricted = { message: "You're not allowed in here!" };
+    expect(response.status).toEqual(400);
+    expect(response.body).toEqual(restricted);
+    expect(response.type).toEqual("application/json");
+  });
+});
