04cad101663287f0da04d63dabd7cba3b0c03408
unknown <will.ediger@gmail.com>
2019-09-12 18:34:58 -0500

    bypass restricted middleware check if AUTH_ENV is in testing mode

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,10 +1,11 @@
-
+require("dotenv").config();
 // check request body session.loggedIn boolean
 // this is set on auth-router on the login endpoint
 module.exports = (req, res, next) => {
-    const sesh = req.session;
-    console.log("running auth middleware");
-    (sesh && (sesh.loggedIn === true))
+  const sesh = req.session;
+  console.log("running auth middleware");
+  //bypass restricted middleware check if AUTH_ENV is in testing mode
+  (sesh && sesh.loggedIn === true) || process.env.AUTH_ENV === "testing"
     ? next()
-    : res.status(400).json({message: "You're not allowed in here!"});
-  }
+    : res.status(400).json({ message: "You're not allowed in here!" });
+};
