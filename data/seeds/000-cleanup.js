b12afe7b1820441777808568be403a97d6058434
unknown <will.ediger@gmail.com>
2019-09-12 21:09:45 -0500

    testing different syntax for seed cleanup

--- data/seeds/000-cleanup.js
+++ data/seeds/000-cleanup.js
@@ -1,10 +1,16 @@
 exports.seed = async function(knex) {
   try {
-    await knex.raw("TRUNCATE TABLE Habit_Tracking RESTART IDENTITY CASCADE");
-    await knex.raw("TRUNCATE TABLE User_Habits RESTART IDENTITY CASCADE");
-    await knex.raw("TRUNCATE TABLE User_Categories RESTART IDENTITY CASCADE");
-    await knex.raw("TRUNCATE TABLE Users RESTART IDENTITY CASCADE");
-    await knex.raw("TRUNCATE TABLE Categories RESTART IDENTITY CASCADE");
+    await knex.schema.raw(
+      "TRUNCATE TABLE Habit_Tracking RESTART IDENTITY CASCADE"
+    );
+    await knex.schema.raw(
+      "TRUNCATE TABLE User_Habits RESTART IDENTITY CASCADE"
+    );
+    await knex.schema.raw(
+      "TRUNCATE TABLE User_Categories RESTART IDENTITY CASCADE"
+    );
+    await knex.schema.raw("TRUNCATE TABLE Users RESTART IDENTITY CASCADE");
+    await knex.schema.raw("TRUNCATE TABLE Categories RESTART IDENTITY CASCADE");
     // await knex.truncate("Habit_Tracking");
     // await knex.truncate("User_Habits");
     // await knex.truncate("User_Categories");
