4c34dc6e4f88fde82da4a09058c0823ce1a8cd6b
unknown <will.ediger@gmail.com>
2019-09-12 21:07:24 -0500

    testing seeding cleanup method

--- data/seeds/000-cleanup.js
+++ data/seeds/000-cleanup.js
@@ -1,10 +1,15 @@
 exports.seed = async function(knex) {
   try {
-    await knex.truncate("Habit_Tracking");
-    await knex.truncate("User_Habits");
-    await knex.truncate("User_Categories");
-    await knex.truncate("Users");
-    await knex.truncate("Categories");
+    await knex.raw("TRUNCATE TABLE Habit_Tracking RESTART IDENTITY CASCADE");
+    await knex.raw("TRUNCATE TABLE User_Habits RESTART IDENTITY CASCADE");
+    await knex.raw("TRUNCATE TABLE User_Categories RESTART IDENTITY CASCADE");
+    await knex.raw("TRUNCATE TABLE Users RESTART IDENTITY CASCADE");
+    await knex.raw("TRUNCATE TABLE Categories RESTART IDENTITY CASCADE");
+    // await knex.truncate("Habit_Tracking");
+    // await knex.truncate("User_Habits");
+    // await knex.truncate("User_Categories");
+    // await knex.truncate("Users");
+    // await knex.truncate("Categories");
   } catch (err) {
     console.log(err);
   }
