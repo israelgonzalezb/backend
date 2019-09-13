b56f115cb6c98ea7a7fb4123e0751712c7d32281
unknown <will.ediger@gmail.com>
2019-09-12 20:38:56 -0500

    removed foreign key checks during seeding

--- data/seeds/000-cleanup.js
+++ data/seeds/000-cleanup.js
@@ -1,10 +1,12 @@
 exports.seed = async function(knex) {
   try {
+    await knex.raw("SET foreign_key_checks = 0;");
     await knex.truncate("Habit_Tracking");
     await knex.truncate("User_Habits");
     await knex.truncate("User_Categories");
     await knex.truncate("Users");
     await knex.truncate("Categories");
+    await knex.raw("SET foreign_key_checks = 1;");
   } catch (err) {
     console.log(err);
   }
