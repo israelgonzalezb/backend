bc6cfd9ba5224fe4fa89e77ffaf516ca12ff80e3
unknown <will.ediger@gmail.com>
2019-09-12 20:10:20 -0500

    removed unnecessary if statements

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -3,18 +3,9 @@ require("dotenv").config();
 // this is set on auth-router on the login endpoint
 module.exports = (req, res, next) => {
   const sesh = req.session;
-  console.log("running auth middleware");
-  if (sesh && sesh.loggedIn === true) {
-    console.log("logged in");
-    next();
-  } else {
-    if (process.env.AUTH_ENV === "testing") {
-      console.log("got thru testing");
-      next();
-    } else {
-      res.status(400).json({
+  (sesh && sesh.loggedIn === true) || process.env.AUTH_ENV === "testing"
+    ? next()
+    : res.status(400).json({
         message: "You're not allowed in here!"
       });
-    }
-  }
 };
