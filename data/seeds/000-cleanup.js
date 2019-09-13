096f1c43baf80c059818a2d36f84190854b1a237
unknown <will.ediger@gmail.com>
2019-09-12 20:45:09 -0500

    another attempt to truncate tables during seeding in postgres

--- data/seeds/000-cleanup.js
+++ data/seeds/000-cleanup.js
@@ -1,12 +1,8 @@
 exports.seed = async function(knex) {
   try {
-    await knex.raw("SET foreign_key_checks = 0;");
-    await knex.truncate("Habit_Tracking");
-    await knex.truncate("User_Habits");
-    await knex.truncate("User_Categories");
-    await knex.truncate("Users");
-    await knex.truncate("Categories");
-    await knex.raw("SET foreign_key_checks = 1;");
+    await knex.schema.raw(
+      "TRUNCATE TABLE Habit_Tracking, User_Habits, User_Categories, Users, Categories CASCADE"
+    );
   } catch (err) {
     console.log(err);
   }
