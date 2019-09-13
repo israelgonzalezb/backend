570d4b5ac262593447c1e3d2382bb067262bf42e
unknown <will.ediger@gmail.com>
2019-09-12 21:15:41 -0500

    testing another cleanup method

--- data/seeds/000-cleanup.js
+++ data/seeds/000-cleanup.js
@@ -1,21 +1,7 @@
 exports.seed = async function(knex) {
   try {
-    await knex.schema.raw(
-      "TRUNCATE TABLE Habit_Tracking RESTART IDENTITY CASCADE"
-    );
-    await knex.schema.raw(
-      "TRUNCATE TABLE User_Habits RESTART IDENTITY CASCADE"
-    );
-    await knex.schema.raw(
-      "TRUNCATE TABLE User_Categories RESTART IDENTITY CASCADE"
-    );
-    await knex.schema.raw("TRUNCATE TABLE Users RESTART IDENTITY CASCADE");
-    await knex.schema.raw("TRUNCATE TABLE Categories RESTART IDENTITY CASCADE");
-    // await knex.truncate("Habit_Tracking");
-    // await knex.truncate("User_Habits");
-    // await knex.truncate("User_Categories");
-    // await knex.truncate("Users");
-    // await knex.truncate("Categories");
+    await knex.migrate.rollback();
+    await knex.migrate.latest();
   } catch (err) {
     console.log(err);
   }
