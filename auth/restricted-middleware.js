23f62354b967a8a1b21407dbb78cba0a25ab43df
unknown <will.ediger@gmail.com>
2019-09-12 19:19:55 -0500

    fixing bypass of restricted middleware

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -2,20 +2,17 @@ require("dotenv").config();
 // check request body session.loggedIn boolean
 // this is set on auth-router on the login endpoint
 module.exports = (req, res, next) => {
-  //bypass restricted middleware check if AUTH_ENV is in testing mode.
-  if (process.env.AUTH_ENV === "testing") {
-    next();
-    res.status(400).json({
-      message: "supposedly we hit next()",
-      env: process.env.AUTH_ENV
-    });
-  }
   const sesh = req.session;
   console.log("running auth middleware");
-  sesh && sesh.loggedIn === true
-    ? next()
-    : res.status(400).json({
-        message: "You're not allowed in here!",
-        env: process.env.AUTH_ENV
+  if (sesh && sesh.loggedIn === true) {
+    next();
+  } else {
+    if (process.env.AUTH_ENV === "testing") {
+      next();
+    } else {
+      res.status(400).json({
+        message: "You're not allowed in here!"
       });
+    }
+  }
 };
