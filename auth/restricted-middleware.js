253ce6e9bc5a0a1547ebb9fe7e551b6fa27e08a2
unknown <will.ediger@gmail.com>
2019-09-12 19:04:45 -0500

    added another message for debugging heroku/postgres

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -5,15 +5,17 @@ module.exports = (req, res, next) => {
   //bypass restricted middleware check if AUTH_ENV is in testing mode.
   if (process.env.AUTH_ENV === "testing") {
     next();
+    res.status(400).json({
+      message: "supposedly we hit next()",
+      env: process.env.AUTH_ENV
+    });
   }
   const sesh = req.session;
   console.log("running auth middleware");
   sesh && sesh.loggedIn === true
     ? next()
-    : res
-        .status(400)
-        .json({
-          message: "You're not allowed in here!",
-          env: process.env.AUTH_ENV
-        });
+    : res.status(400).json({
+        message: "You're not allowed in here!",
+        env: process.env.AUTH_ENV
+      });
 };
